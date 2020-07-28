b29dcffe52335d39656e0c1f585aaf75a4e37a97
Ashley <fowlerae@rose-hulman.edu>
2020-07-27 21:01:56 -0400

    adding notes recycler view

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/note/NoteAdapter.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/note/NoteAdapter.kt
@@ -26,7 +26,7 @@ class NoteAdapter(val context: Context, uid: String, projectId: String) : Recycl
 
     fun addSnapshotListener() {
         listenerRegistration = notesRef
-            .orderBy(Project.LAST_TOUCHED_KEY, Query.Direction.ASCENDING)
+            .orderBy(Note.CREATED_KEY, Query.Direction.ASCENDING)
             .addSnapshotListener { querySnapshot, e ->
                 if (e != null) {
                     Log.w(Constants.TAG, "listen error", e)
