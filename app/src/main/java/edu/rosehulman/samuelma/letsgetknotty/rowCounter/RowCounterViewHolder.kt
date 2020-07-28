f5afda71a8de7232149d207faafb184d2ee15a31
Ashley <fowlerae@rose-hulman.edu>
2020-07-27 22:50:26 -0400

    fixing add row counter and add note

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/RowCounterViewHolder.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/RowCounterViewHolder.kt
@@ -9,12 +9,14 @@ import edu.rosehulman.samuelma.letsgetknotty.R
 
 class RowCounterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)  {
 
+    private val rowCounterName : TextView = itemView.findViewById(R.id.row_counter_name)
     private val rowTextView: TextView = itemView.findViewById(R.id.current_row_text_view)
     private val upImageView : ImageView = itemView.findViewById(R.id.row_counter_increase_button)
     private val downImageView : ImageView = itemView.findViewById(R.id.row_counter_decrease_button)
   //  private var cardView: CardView = itemView.row_card_view
 
     fun bind(rowCounter: RowCounter) {
+        rowCounterName.text = rowCounter.name
         rowTextView.text = rowCounter.currentRow.toString()
         upImageView.setOnClickListener {
             rowCounter.increaseRow()
