109e1eeeb70198732a14ec55912975eedbd9b460
Ashley <fowlerae@rose-hulman.edu>
2020-08-02 17:46:37 -0400

    setting up logic for create pattern fragment

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/project/PatternAdapter.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/project/PatternAdapter.kt
@@ -110,7 +110,7 @@ class PatternAdapter(val context: Context, uid: String, projectId: String, var l
             if(position < 0) {
                 pattern.imageUrl = "https://cdn.shopify.com/s/files/1/0032/0025/4021/products/ilia_01_182d4112-7a3f-4057-807e-7f9cc68bfe79_480x480.jpg?v=1571710489"
                 add(pattern)
-                listener?.onAddPatternSelected(pattern)
+                listener?.onAddPatternSelected(patterns[0])
             } else {
                 edit(position, name, rowsInRepeat,stitchesInRepeat,totalRows,totalStitches)
             }
