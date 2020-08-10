eccc48484ca0ff5bb20ff0076f0c51128a2d66c4
Ashley <fowlerae@rose-hulman.edu>
2020-08-10 18:56:22 -0400

    trying to fix issue of not saving grid in the right location

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/project/PatternAdapter.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/project/PatternAdapter.kt
@@ -118,7 +118,7 @@ class PatternAdapter(
                 pattern.imageUrl =
                     "https://cdn.shopify.com/s/files/1/0032/0025/4021/products/ilia_01_182d4112-7a3f-4057-807e-7f9cc68bfe79_480x480.jpg?v=1571710489"
                 add(pattern)
-
+                Log.d(Constants.TAG, pattern.toString())
                 listener?.onAddPatternSelected(patterns[findPattern(pattern)])
             } else {
                 edit(position, name, rowsInRepeat, stitchesInRepeat, totalRows, totalStitches)
