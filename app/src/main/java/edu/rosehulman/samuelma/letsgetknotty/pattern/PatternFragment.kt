dc095cefc57911269924bd6ad62f87874a92762b
Mary Ashley Samuelson <samuelma@rose-hulman.edu>
2020-08-11 02:31:55 -0500

    pattern title now displays, although there is still the issue that the previous pattern data is loading

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternFragment.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -54,6 +55,9 @@ class PatternFragment : Fragment() {
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = adapter
         adapter.addSnapshotListener()
+
+        view.findViewById<TextView>(R.id.pattern_title_view).text = pattern.name
+
         return view
     }
 
