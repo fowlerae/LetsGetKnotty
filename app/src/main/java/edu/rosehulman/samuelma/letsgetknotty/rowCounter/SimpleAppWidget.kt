ef3a8b638251a5b6c8f7afb1fd0af7611d49a56d
Ashley <fowlerae@rose-hulman.edu>
2020-08-12 22:11:34 -0400

    more formatting for the widget layout

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/SimpleAppWidget.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/SimpleAppWidget.kt
@@ -14,6 +14,7 @@ class SimpleAppWidget : AppWidgetProvider() {
     private val ACTION_INCREASE_BUTTON = "ACTION_INCREASE_BUTTON"
     private val ACTION_DECREASE_BUTTON = "ACTION_DECREASE_BUTTON"
     private lateinit var rowCounter : RowCounter
+    private var count : Int = 0
 
 //    override fun onEnabled(context: Context?) {
 //        super.onEnabled(context)
@@ -39,48 +40,51 @@ class SimpleAppWidget : AppWidgetProvider() {
         context: Context, appWidgetManager: AppWidgetManager,
         appWidgetId: Int
     ) {
-//        // make the RemoteViews object
-//        val views = RemoteViews(context.packageName, R.layout.simple_app_widget)
-//
-//        // make the intents and assign them to the buttons
-//        val increaseIntentButton = Intent(context, SimpleAppWidget::class.java)
-//        increaseIntentButton.action = ACTION_INCREASE_BUTTON
-//
-//        val increasePendingIntentButton =
-//            PendingIntent.getBroadcast(context, 0, increaseIntentButton, PendingIntent.FLAG_UPDATE_CURRENT)
-//
-//        views.setOnClickPendingIntent(R.id.widget_row_counter_increase_button, increasePendingIntentButton)
-//
-//        val decreaseIntentButton = Intent(context, SimpleAppWidget::class.java)
-//        decreaseIntentButton.action = ACTION_DECREASE_BUTTON
-//
-//        val decreasePendingIntentButton =
-//            PendingIntent.getBroadcast(context, 0, increaseIntentButton, PendingIntent.FLAG_UPDATE_CURRENT)
-//
-//        views.setOnClickPendingIntent(R.id.widget_row_counter_decrease_button, decreasePendingIntentButton)
-//
-//        // tell the manager to update the widget
-//        appWidgetManager.updateAppWidget(appWidgetId, views)
+        // make the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.simple_app_widget)
+
+        // make the intents and assign them to the buttons
+        val increaseIntentButton = Intent(context, SimpleAppWidget::class.java)
+        increaseIntentButton.action = ACTION_INCREASE_BUTTON
+
+        val increasePendingIntentButton =
+            PendingIntent.getBroadcast(context, 0, increaseIntentButton, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        views.setOnClickPendingIntent(R.id.widget_row_counter_increase_button, increasePendingIntentButton)
+
+        val decreaseIntentButton = Intent(context, SimpleAppWidget::class.java)
+        decreaseIntentButton.action = ACTION_DECREASE_BUTTON
+
+        val decreasePendingIntentButton =
+            PendingIntent.getBroadcast(context, 0, increaseIntentButton, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        views.setOnClickPendingIntent(R.id.widget_row_counter_decrease_button, decreasePendingIntentButton)
+
+        // tell the manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
 
-//        val views = RemoteViews(context.packageName, R.layout.simple_app_widget)
-//        if(ACTION_INCREASE_BUTTON == intent.action) {
-//            // change values in
-//            rowCounter.increaseRow()
-//
-//            views.setTextViewText(R.id.widget_current_row, rowCounter.currentRow.toString())
-//
-//        } else if(ACTION_DECREASE_BUTTON == intent.action) {
-//            rowCounter.decreaseRow()
-//            views.setTextViewText(R.id.widget_current_row, rowCounter.currentRow.toString())
-//        }
-//
-//            val appWidget = ComponentName(context, SimpleAppWidget::class.java)
-//            val appWidgetManager = AppWidgetManager.getInstance(context)
-//            appWidgetManager.updateAppWidget(appWidget, views)
+        val views = RemoteViews(context.packageName, R.layout.simple_app_widget)
+        if(ACTION_INCREASE_BUTTON == intent.action) {
+            // change values in
+           // rowCounter.increaseRow()
+            // views.setTextViewText(R.id.widget_current_row, rowCounter.currentRow.toString())
+            count++
+            views.setTextViewText(R.id.widget_current_row, count.toString())
+
+        } else if(ACTION_DECREASE_BUTTON == intent.action) {
+           // rowCounter.decreaseRow()
+           // views.setTextViewText(R.id.widget_current_row, rowCounter.currentRow.toString())
+            count--
+            views.setTextViewText(R.id.widget_current_row, count.toString())
+        }
+
+            val appWidget = ComponentName(context, SimpleAppWidget::class.java)
+            val appWidgetManager = AppWidgetManager.getInstance(context)
+            appWidgetManager.updateAppWidget(appWidget, views)
     }
 
 
