f71a96414517d23581dafb413c63ca9bebc1e18f
Ashley <fowlerae@rose-hulman.edu>
2020-08-15 19:00:39 -0400

    adding log messages

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/createPattern/CreatePatternAdapter.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/createPattern/CreatePatternAdapter.kt
@@ -109,11 +109,12 @@ class CreatePatternAdapter(val context: Context, uid: String, project: Project, 
 
     fun updateStitch(position: Int) {
         //  add delays here
-        Log.d(Constants.TAG, "update stitch Stitch: ${stitch}")
-        Log.d(Constants.TAG, "update stitch  Button Color: ${color}")
-        rectangles[position].image = this.stitch!!
+
         val handler = Handler()
         handler.postDelayed({
+            Log.d(Constants.TAG, "update stitch Stitch: ${stitch}")
+            Log.d(Constants.TAG, "update stitch  Button Color: ${color}")
+            rectangles[position].image = this.stitch!!
             gridRef.document(rectangles[position].id).set(rectangles[position])
         }, 50)
 
