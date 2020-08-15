f1c736be541eb28641884d274d8aac4bb36f7940
Ashley <fowlerae@rose-hulman.edu>
2020-08-14 21:24:08 -0400

    trying to pull name from rowCounter

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/WidgetService.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/rowCounter/WidgetService.kt
@@ -164,6 +164,7 @@ class WidgetService : Service() {
 
         // Reaches the view on widget and displays the number
         val view = RemoteViews(packageName, R.layout.simple_app_widget)
+        view.setTextViewText(R.id.widget_row_counter_name, rowCounter?.name)
         view.setTextViewText(R.id.widget_current_row, rowCounter?.currentRow.toString())
         val theWidget = ComponentName(this, SimpleAppWidget::class.java)
         val manager = AppWidgetManager.getInstance(this)
