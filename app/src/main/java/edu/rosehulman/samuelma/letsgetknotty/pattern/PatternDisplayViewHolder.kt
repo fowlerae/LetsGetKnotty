e85676167e6183e7e1ff5f0e9889c87de1c7e32a
Ashley <fowlerae@rose-hulman.edu>
2020-08-15 14:06:25 -0400

    bug fixes with the add stitch button

--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternDisplayViewHolder.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternDisplayViewHolder.kt
@@ -1,6 +1,7 @@
 package edu.rosehulman.samuelma.letsgetknotty.pattern
 
 import android.view.View
+import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import edu.rosehulman.samuelma.letsgetknotty.createPattern.Grid
@@ -10,8 +11,12 @@ import kotlinx.android.synthetic.main.create_pattern_grid_view.view.*
 class PatternDisplayViewHolder(itemView: View, private val adapter: PatternDisplayAdapter): RecyclerView.ViewHolder(itemView) {
 
     private val cardView :CardView = itemView.grid_card_view
+    private val imageView : ImageView = itemView.grid_stitch_image
 
     fun bind(grid: Grid) {
         cardView.setCardBackgroundColor(grid.color)
+        if(grid.image != null) {
+            imageView.setImageResource(grid.image!!)
+        }
     }
 }
