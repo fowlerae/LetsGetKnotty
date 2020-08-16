adc290a957e222031d0659ecffe691e2c2ee71ae
Ashley <fowlerae@rose-hulman.edu>
2020-08-15 21:34:13 -0400

    adding calculate yardage feature

---
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/calculateYardage/CalculateYardageFragment.kt
@@ -0,0 +1,107 @@
+package edu.rosehulman.samuelma.letsgetknotty.calculateYardage
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import edu.rosehulman.samuelma.letsgetknotty.R
+import edu.rosehulman.samuelma.letsgetknotty.createPattern.PatternDisplayAdapter
+import edu.rosehulman.samuelma.letsgetknotty.pattern.*
+import edu.rosehulman.samuelma.letsgetknotty.project.Project
+
+private const val ARG_PATTERN = "pattern"
+private const val ARG_UID = "uid"
+private const val ARG_PROJECT = "project"
+
+class CalculateYardageFragment : Fragment() {
+
+
+    private lateinit var pattern: Pattern
+    private lateinit var uid: String
+    private lateinit var project : Project
+    private lateinit var adapter: PatternDisplayAdapter
+    companion object {
+        @JvmStatic
+        fun newInstance(uid :String, pattern: Pattern, project: Project) =
+            PatternFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_PATTERN, pattern)
+                    putString(ARG_UID,uid)
+                    putParcelable(ARG_PROJECT,project)
+                }
+            }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pattern = arguments!!.getParcelable(ARG_PATTERN)!!
+        project = arguments!!.getParcelable(ARG_PROJECT)!!
+        uid = arguments!!.getString(ARG_UID).toString()
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view : View = inflater.inflate(R.layout.fragment_pattern, container, false)
+        val button : Button = view.findViewById(R.id.calculate_yardage_button)
+        button.setOnClickListener {
+            val message = calculate(view)
+            calculateDialog(message)
+        }
+
+        return view
+    }
+
+
+    private fun calculateDialog(message: String) {
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle("Estimated Skeins Needed For Pattern")
+        builder.setMessage(message)
+        builder.setNegativeButton("Enter Information Again") { _, _ ->
+            // remove(position)
+        }
+        builder.setPositiveButton("Done") { _, _ ->
+            // remove(position)
+
+        }
+        builder.show()
+    }
+
+    fun calculate(view: View): String {
+        val desiredHeightString = view.findViewById<EditText>(R.id.calc_desired_height).toString()
+        val desiredWidthString : String = view.findViewById<EditText>(R.id.calc_desired_width).toString()
+
+        val projectHeightString : String = view.findViewById<EditText>(R.id.calc_project_height).toString()
+        val projectWidthString : String = view.findViewById<EditText>(R.id.calc_project_width).toString()
+
+        val yardsUsedString : String = view.findViewById<EditText>(R.id.calc_yards_used).toString()
+        val yardsPerSkeinString : String = view.findViewById<EditText>(R.id.calc_yarns_per_skein).toString()
+
+        if(desiredHeightString != "" || desiredWidthString != "" || projectHeightString == ""
+            || projectWidthString != "" || yardsUsedString != "" || yardsPerSkeinString != "") {
+
+            val dHeight : Double = desiredHeightString.toInt().toDouble()
+            val dWidth : Double = desiredWidthString.toInt().toDouble()
+            val pHeight : Double = projectHeightString.toInt().toDouble()
+            val pWidth : Double = projectWidthString.toInt().toDouble()
+            val yUsed : Double = yardsUsedString.toInt().toDouble()
+            val ySkein : Double = yardsPerSkeinString.toInt().toDouble()
+
+            val yardageRatio : Double = yUsed / ySkein
+            val desiredArea : Double = dHeight * dWidth
+            val actualArea : Double = pHeight * pWidth
+            val areaRatio : Double = desiredArea/actualArea
+            val skeinsNeeded : Double = areaRatio * yardageRatio
+
+            return skeinsNeeded.toString()
+        }
+        return "Please fill out all fields"
+    }
+}
--- app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternFragment.kt
+++ app/src/main/java/edu/rosehulman/samuelma/letsgetknotty/pattern/PatternFragment.kt
@@ -4,16 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.rosehulman.samuelma.letsgetknotty.R
+import edu.rosehulman.samuelma.letsgetknotty.calculateYardage.CalculateYardageFragment
 import edu.rosehulman.samuelma.letsgetknotty.createPattern.PatternDisplayAdapter
 import edu.rosehulman.samuelma.letsgetknotty.project.Project
+import edu.rosehulman.samuelma.letsgetknotty.project.ProjectFragment
 
 
-private const val ARG_PATTERN = "pattern"
+const val ARG_PATTERN = "pattern"
 private const val ARG_UID = "uid"
 private const val ARG_PROJECT = "project"
 
@@ -55,9 +58,25 @@ class PatternFragment : Fragment() {
         adapter.addSnapshotListener()
         val name : TextView = view.findViewById(R.id.pattern_fragment_name_text_view)
         name.text = pattern.name
+
+        val button : Button = view.findViewById(R.id.calculate_yardage_button)
+        button.setOnClickListener {
+            switchToYardageFragment()
+        }
+
         return view
     }
 
+    private fun switchToYardageFragment() {
+        val fragment = CalculateYardageFragment.newInstance(uid, pattern, project)
+        val fm = fragmentManager
+        val ft = fm?.beginTransaction()
+        if (ft != null) {
+            ft.replace(R.id.fragment_container, fragment)
+            ft.addToBackStack("yardage")
+            ft.commit()
+        }
+    }
 
 
 
